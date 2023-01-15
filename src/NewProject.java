237fc04f7d5fac6455a8ac8d110092c766e9d59e
nika_nest <nika_nesterova@bk.ru>
2023-01-15 18:34:30 +0100

    Initial commit

---
+++ src/NewProject.java
@@ -0,0 +1,12 @@
+public class NewProject {
+    public static void main(String[] args) {
+
+        int ticketPrice = 5000;
+        int bonusMilesPerRubles = 20;
+
+        int bonusPerTicket = ticketPrice / bonusMilesPerRubles;
+
+        System.out.println("Начислено милей: " + bonusPerTicket);
+
+    }
+}
